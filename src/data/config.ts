8f10607c112c6fc46aafc1023a673ef54449faf2
joaosmoreira <37328807+joaosmoreira@users.noreply.github.com>
2026-02-27 01:30:42 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/data/config.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "João Moreira",
+  role: "Preparador de Obra de Estruturas Metálicas @ Garsteel",
+  bio: "Com uma década de experiência multidisciplinar em design gráfico, marketing digital e desenvolvimento web, transitei para a vertente técnica. Após concluir o bootcamp de programação na Code for All, o panorama revelou-se conturbado. Contudo, essa base abriu-me portas para uma nova área com forte necessidade: a preparação de obra. Ingressei no curso e surgiu a oportunidade nas estruturas metálicas, algo que me cativou pelo trabalho minucioso e de grande detalhe.",
+  emailjs: {
+    serviceId: "YOUR_SERVICE_ID",
+    templateId: "YOUR_TEMPLATE_ID",
+    publicKey: "YOUR_PUBLIC_KEY",
+  }
+};
---
+++ src/data/education.ts
@@ -0,0 +1,32 @@
+export const education = [
+    {
+        institution: "CICCOPN",
+        course: "Técnico Preparador de Obra – CAD e Modelação BIM",
+        duration: "nov de 2024 - jul de 2025",
+        skills: ["AutoCAD", "AutoCAD Civil 3D", "Revit", "BIM"],
+    },
+    {
+        institution: "Code for All_",
+        course: "A 14-Week Intensive && Immersive Full Stack Programming Bootcamp",
+        duration: "jan de 2023 - abr de 2023",
+        skills: ["Git", "Java", "Spring Framework", "SQL", "JavaScript", "React", "HTML", "CSS"],
+    },
+    {
+        institution: "MODATEX",
+        course: "Marketing Technician, Marketing Management",
+        duration: "2013 - 2014",
+        skills: ["Marketing", "Gestão"],
+    },
+    {
+        institution: "Escola Profissional do Comercio Externo do Porto",
+        course: "Multimedia Technician, Web/Multimedia Management and Webmaster",
+        duration: "2007 - 2009",
+        skills: ["Programming", "Web Development", "HTML", "CSS", "Action Script", "Flash", "Fotografia", "Video Editing", "Motion Design", "Graphic Design"],
+    },
+    {
+        institution: "Escola Secundária D. Afonso Henriques",
+        course: "Information Technology",
+        duration: "2001 - 2003",
+        skills: ["Programming", "HTML", "CSS", "Pascal", "C"],
+    }
+];
